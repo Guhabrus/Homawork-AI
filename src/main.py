be915d8bf15b4c4f8ac4b1f1a81f38b9bfc13609
Максим Кукушкин <cnegbyj99@mail.ru>
2022-12-18 14:15:58 +0300

    аккуратно кладу в папочку

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import dowload as down
+import os
 
+folder_m_name = "model/"
+model_name = "model.xyz"
 
 
 if (__name__ == '__main__'):
+
     
-    if (down.dowload_file("https://disk.yandex.ru/d/2pAan4GrthX-uw",'test.obj' ) ):
+    if(not os.path.exists(folder_m_name)):
+        os.mkdir(folder_m_name)    
+
+    if (down.dowload_file("https://disk.yandex.ru/d/N_UY4lDG6TIRTw",folder_m_name + model_name ) ):
         ... 
 
     # down.download_yandex()
