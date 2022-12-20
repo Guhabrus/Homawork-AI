345222a2b76ff19a572637911891e422053561ca
Максим Кукушкин <cnegbyj99@mail.ru>
2022-12-20 21:03:22 +0300

    набыдлокодил

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ folder_m_name = "../model/"
 model_name = "model.xyz"
 
 
+import numpy as np
+
 if (__name__ == '__main__'):
 
     
@@ -16,4 +18,15 @@ if (__name__ == '__main__'):
     # if (down.dowload_file("https://disk.yandex.ru/d/N_UY4lDG6TIRTw",folder_m_name + model_name ) ):
     #     ... 
 
-    alg.data_process(folder_m_name + model_name)
+    # alg.data_process(folder_m_name + model_name)
+
+    x = np.arange(10)
+    y = np.arange(10, 20)
+    
+
+    
+    x = np.append(x,y, axis=0)
+    x = np.reshape(x, (2,len(y)))
+    
+    print(x)
+    
